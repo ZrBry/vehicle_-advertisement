d367cb5391665c1da0b65e34a751d5e1e8e079fd
ZrBry <zrbrzr@hotmail.com>
2024-09-10 02:53:37 -0600

    Se crearon graficos del proyecto, falta corrregir el codigo de app.py

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
+import streamlit as st
 import pandas as pd
-import streamlit as st 
-import plotly.express as px
-import matplotlib as plt
+import plotly_express
+car_data = pd.read_csv("vehicles_us.csv")
+st.header("Vehicle Advertisement")
+first_button = st.button("Construir histograma")
+second_button = st.button("construir grafico de dispersión")
 
-data = pd.read_csv("./vehicles_us.csv")
-#print(data.info())
-#print(data.sample(10))
-#print(data["transmission"].sample(20))
+if first_button:
+    st.write("Creacion de un histograma para el conjunto de datos de anuncios de venta de coches")
 
-fig = px.histogram(data, "transmission")
-fig.show()
+    fig = px.histogram(car_data, x="odometer")
 
-st.plotly_chart(fig)
+    st.plotly_chart(fig, use_container_width=True)
+
+if second_button:
+    st.write("Creacion de un grafico de dispersión para el conjunto de datos de anuncios de venta de coches")
+
+    fig = px.histogram(car_data, x="odometer", y="price")
+
+    st.plotly_chart(fig, use_container_width=True)
