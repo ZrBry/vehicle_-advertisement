947a186f2451ef2ef93949a3bbce153f2e60febd
ZrBry <zrbrzr@hotmail.com>
2024-09-12 02:11:41 -0600

    se completa el codigo en app.py

--- app.py
+++ app.py
@@ -6,25 +6,25 @@ car_data = pd.read_csv("vehicles_us.csv")
 
 st.header("Vehicle Advertisement")
 
-first_button = st.button("Construir histograma")
-second_button = st.button("Construir histograma")
-third_button = st.button("Construir grafico de dispersión")
+button = st.button("Construir histograma")
 
-if first_button:
+second_button = st.button("Construir grafico de dispersión")
+
+if button:
     st.write("Creacion de un histograma sobre la transmision para el conjunto de datos de anuncios de venta de coches")
 
     fig = px.histogram(car_data, x="transmission")
 
     st.plotly_chart(fig, use_container_width=True)
 
-if second_button:
+if button:
     st.write("Creacion de un histograma sobre el kilometraje para el conjunto de datos de anuncios de venta de coches")
 
     fig = px.histogram(car_data, x="odometer")
 
     st.plotly_chart(fig, use_container_width=True)
 
-if third_button:
+if second_button:
     st.write("Creacion de un grafico de dispersión para el conjunto de datos de anuncios de venta de coches")
 
     fig = px.scatter(car_data, x="odometer", y="price")
